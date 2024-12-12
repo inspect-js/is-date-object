4f1d9b3d1908d5bfd536128fe8fa49e16fd48761
Jordan Harband <ljharb@gmail.com>
2024-12-12 14:18:18 -0800

    [New] add types

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var callBound = require('call-bound');
 
 var getDay = callBound('Date.prototype.getDay');
+/** @type {import('.')} */
 var tryDateObject = function tryDateGetDayCall(value) {
 	try {
 		getDay(value);
@@ -12,10 +13,12 @@ var tryDateObject = function tryDateGetDayCall(value) {
 	}
 };
 
+/** @type {(value: unknown) => string} */
 var toStr = callBound('Object.prototype.toString');
 var dateClass = '[object Date]';
 var hasToStringTag = require('has-tostringtag/shams')();
 
+/** @type {import('.')} */
 module.exports = function isDateObject(value) {
 	if (typeof value !== 'object' || value === null) {
 		return false;
